f062c3519eae07efba3c10d22e80b3ae48e00ba7
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-04 08:49:54 -0600

    Added dice roll guessing game.

---
+++ app/views/roll-dice.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dice Roll</title>
+</head>
+<body>
+
+	<h2>You guessed <?= $guess; ?>.</h2>
+	<h2>You rolled <?= $roll; ?>.</h2>
+	<h2>You <?= $success; ?></h2>
+</body>
+</html>
