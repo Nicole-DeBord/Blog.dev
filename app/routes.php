61a03ce820f7e1049b4912a41b82ad35779566a1
Nicole DeBord <a.nicole.debord@gmail.com>
2015-02-04 08:52:40 -0600

    Routes updated to support relevant views.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,50 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+
+Route::get('say-hello/{name}/{age}', function($name, $age) {
+
+	return "Hello $name! I hear you're $age years old.";
+
 });
+
+
+// Dice Roll Game: 
+
+Route::get('diceroll/{guess}', function($guess) {
+
+	$success = '';
+	$diceRoll = rand(1, 6);
+
+	if ($diceRoll == $guess) {
+		$success = "are successful! Bask in your glory!";
+	} else {
+		$success = "fail at life. :(";
+	}
+
+	$message = "You guessed {$guess}, you rolled $diceRoll! You {$success}";
+
+	$data = [
+		'roll'    => $diceRoll,
+		'guess'   => $guess,
+		'success' => $success
+	];
+
+	return View::make('roll-dice')->with($data);
+});
+
+
+Route::get('/', 'HomeController@showWelcome');
+
+Route::get('say-hello/{name}/{age}', 'HomeController@sayHello');
+
+Route::get('resume', 'HomeController@getResume');
+
+Route::get('portfolio', 'HomeController@getPortfolio');
+
+Route::resource('posts', 'PostsController');
+
+Route::get('parks', 'HomeController@getParks');
+
+Route::get('whack', 'HomeController@getWhack');
+
